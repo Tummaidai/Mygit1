373ea4ac7477498c8a4dff14568685b45d72f3d1
Tummaidai <watsayodpoon122@gmail.com>
2024-01-24 11:55:47 +0700

    Update streamlit01.py

--- streamlit01.py
+++ streamlit01.py
@@ -57,7 +57,7 @@ ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
         shadow=True, startangle=90)
 #ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 
-if(st.button("แสดงข้อมูล")):
+if(st.button("แสดงข้อมูลกราฟวงกลม")):
     st.pyplot(fig1)
     st.button("ไม่แสดงข้อมูล")
 
