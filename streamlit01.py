b1c2a7c6f62f84d7279afc20b5b597e6c536d5a9
Tummaidai <watsayodpoon122@gmail.com>
2024-01-24 10:36:05 +0700

    .

--- streamlit01.py
+++ streamlit01.py
@@ -8,12 +8,12 @@ col1, col2, col3 = st.columns(3)
 
 with col1:
     st.header("Versicolor")
-    st.image("https://en.m.wikipedia.org/wiki/File:Iris_versicolor_3.jpg")
+    st.image("./img/800px-Iris_versicolor_3.jpg")
 
 with col2:
     st.header("Verginica")
-    st.image("https://en.m.wikipedia.org/wiki/File:Iris_virginica.jpg")
+    st.image(".img/Iris_virginica.jpg")
 
 with col3:
     st.header("Setosa")
-    st.image("https://species.m.wikimedia.org/wiki/File:Iris_setosa_2.jpg")
+    st.image("./img/661px-Iris_setosa_2.jpg")
